fcec67bc6836328447c1d0dd7152afe553fa1fc6
viroscyber <39602301+viroscyber@users.noreply.github.com>
2018-11-14 10:32:26 +0700

    Add files via upload

---
+++ viros.sh
@@ -0,0 +1,18 @@
+!/usr/xbin/bash
+#codebySY4H
+
+echo "\033[34;1m###################"
+echo "\033[34;1m#  Coded By SY4H  #"
+echo "\033[34;1m#                 #"
+echo "\033[34;1m###################"
+echo "\033[34;1m#01  Spam Whats App#"
+echo "\033[34;1m####################"
+echo
+date | lolcat
+echo "\033[32;1m╭──=>>"
+read -p "╰──────>Pilih NO : " rus
+if [ $rus = 1 ] || [ $rus = 01 ];then
+clear
+php _wa_.php
+fi
+
